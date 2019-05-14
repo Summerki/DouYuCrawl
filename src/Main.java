3fe90f73643028267946b9986eb21cdbceeaa77a
summerki <3136753204@qq.com>
2019-05-14 14:58:52 +0800

    2019-5-14:将弹幕消息写入到txt中完成

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,9 +20,10 @@ public class Main {
 
 
     // 通过正则表达式匹配对象
-    public static void regexMatches(String str){
+    public static void regexMatches(String str) throws IOException {
         String nn;  // 斗鱼用户昵称
         String txt;  // 斗鱼用户弹幕发送内容
+        String danMu;  // 按照指定格式拼接好的str
 
 //        System.out.println(str);  // 原字符串
         String regexStr = "(.*)(nn@=)(.*)(/txt@=)(.*)(/cid@=.*)";
@@ -31,8 +34,21 @@ public class Main {
 //            System.out.println(nn);
             txt = matcher.group(5);
 //            System.out.println(txt);
+
+            danMu = "用户: " + nn + "\t" + "  发送弹幕: " + txt + "\r\n";
+            strToFile(danMu);
             System.out.println("用户: " + nn + "\t" + "  发送弹幕: " + txt);
         }
+    }
+
+
+    public static String filePath = "./DanMuFile/DanMu.txt";
+    public static void strToFile(String str) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(str);
+        bw.close();
+        fw.close();
     }
 
 
